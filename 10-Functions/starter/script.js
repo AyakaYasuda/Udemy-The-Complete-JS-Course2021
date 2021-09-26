445160e5e02726fbef8faa0702741b75a2507834
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-09-25 23:49:03 -0700

    add explanation about setting default params

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  frightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 way to set the default
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    frightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
