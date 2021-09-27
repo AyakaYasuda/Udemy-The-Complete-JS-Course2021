d8991494ce597b72f7fa606a34dac67c9aceb8ca
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-09-27 00:12:50 -0700

    add closure example pt2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -210,6 +210,19 @@ h();
 f();
 console.dir(f);
 
+// closure example #2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boading in ${wait} seconds`);
+};
+boardPassengers(180, 3);
+
 ///////////////////////////////////////
 // Coding Challenge #1
 
