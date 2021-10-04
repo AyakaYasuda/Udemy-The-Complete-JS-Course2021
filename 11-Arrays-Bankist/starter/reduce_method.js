f93e72f951f45236c8d9dbb57d845582fc5e688b
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-10-04 15:12:42 -0700

    add how to figure out the max value by using reduce method

--- 11-Arrays-Bankist/starter/reduce_method.js
+++ 11-Arrays-Bankist/starter/reduce_method.js
@@ -17,3 +17,10 @@ console.log(balanceArrow);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+// figure out the maximum value by using reduce method 
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
