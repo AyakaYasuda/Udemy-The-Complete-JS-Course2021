f36b63486fba69107c8a4669891b985b935a5df3
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-10-05 14:16:33 -0700

    add chaining_methods file and calcDisplaySummary function

---
+++ 11-Arrays-Bankist/starter/chaning_methods.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const euroToUSD = 1.1;
+
+const totalDepositUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * euroToUSD;
+  })
+  //   .map(mov => mov * euroToUSD)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositUSD);
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -73,7 +73,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>
     `;
 
@@ -81,6 +81,41 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((interest, i, arr) => {
+      console.log(arr)
+      return interest >= 1;
+    })
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interest}€`
+};
+calcDisplaySummary(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+calcDisplayBalance(account1.movements);
+
+/*
+check the defference between innerHTML and textContent
+console.log(containerMovements.innerHTML);
+console.log(containerMovements.textContent);
+*/
 
 const createUserName = function (account) {
   account.forEach(function (acc) {
@@ -93,18 +128,6 @@ const createUserName = function (account) {
 };
 createUserName(accounts);
 console.log(accounts);
-
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-calcDisplayBalance(account1.movements);
-
-/*
-check the defference between innerHTML and textContent
-console.log(containerMovements.innerHTML);
-console.log(containerMovements.textContent);
-*/
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
