df2825a17a73cb926ef0a4739330bd6cf0dfc500
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-10-22 16:23:06 -0700

    complete coding challenge #3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -188,5 +188,43 @@ steven.calcAge();
 console.log(steven.__proto__ === PersonProto); // -> true
 
 const sarah = Object.create(PersonProto);
-sarah.init("Sarah", 1979);
+sarah.init('Sarah', 1979);
 sarah.calcAge();
+
+//////////////////////////////////////////////
+// Inheritance between classes constructor functions
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__); 
+console.log(mike.__proto__.__proto__); 
+
+console.log(mike instanceof Student); // -> true
+console.log(mike instanceof Person); // => true
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
