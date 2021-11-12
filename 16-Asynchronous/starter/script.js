563ef2ebd084a228ac436cb3b34f47c6afc6535e
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-11-11 23:56:19 -0800

    removed callback hell from adding neighbour coutry function

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,44 +4,44 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-// const getCountryData = country => {
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
-//   request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
+    const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+            </div>
+        </article>
+      `;
 
-//     const html = `
-//         <article class="country">
-//             <img class="country__img" src="${data.flags.png}" />
-//             <div class="country__data">
-//                 <h3 class="country__name">${data.name}</h3>
-//                 <h4 class="country__region">${data.region}</h4>
-//                 <p class="country__row"><span>👫</span>${(
-//                   +data.population / 1000000
-//                 ).toFixed(1)} people</p>
-//                 <p class="country__row"><span>🗣️</span>${
-//                   data.languages[0].name
-//                 }</p>
-//                 <p class="country__row"><span>💰</span>${
-//                   data.currencies[0].name
-//                 }</p>
-//             </div>
-//         </article>
-//       `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// };
-
-// getCountryData('portugal');
-// getCountryData('USA');
-// getCountryData('germany');
-
+getCountryData('portugal');
+getCountryData('USA');
+getCountryData('germany');
+*/
 const renderCountry = (data, className = '') => {
   const html = `
         <article class="country ${className}">
@@ -123,8 +123,20 @@ console.log(request3); // return promise
 // };
 
 const getCountryData = function (country) {
+  // country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbourCountry = data[0].borders[0];
+
+      if (!neighbourCountry) return;
+
+      // country2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbourCountry}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
-getCountryData('portugal');
+// getCountryData('portugal');
+getCountryData('germany');
