5b2cbf80b30782d703a2e4035bb635b0b6a04d32
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-11-17 13:49:25 -0800

    added lecture about running promises in parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -398,6 +398,8 @@ const whereAmI = async function (country) {
   throw err;
 };
 
+/*
+
 console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
@@ -416,6 +418,8 @@ console.log('1: Will get location');
   console.log('3: Finished getting location');
 })();
 
+*/
+
 //=====================================================================
 // << try... catch >>
 
@@ -426,3 +430,72 @@ console.log('1: Will get location');
 // } catch (err) {
 //   alert(err.message);
 // }
+
+//=====================================================================
+// << Running promises in parallel >>
+
+/*
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(data => data[0].capital));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
+*/
+
+//=====================================================================
+// << Other promise Combinators >>
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+// Promise.any [ES2021] - return the first fulfilled promise
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(res => console.error(err));
